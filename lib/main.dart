f9f68f44a3e5362e0f05ab61eeabe763050dc23a
Naomi <naomi.ndibui@gmail.com>
2020-03-25 09:34:34 +0300

    sizedBox

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,9 @@ class MyApp extends StatelessWidget {
                  color: Colors.white,
                  child: Text('Container 1'),
              ),
+               SizedBox(
+                 height: 20.0,
+               ),
                Container(
                  height:100.0,
                  color: Colors.blue,
