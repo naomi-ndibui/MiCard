665cbb3a1c938c95634c9f3b891a7c933106b73c
Naomi <naomi.ndibui@gmail.com>
2020-03-25 14:08:22 +0300

    fonts and textstyle widget

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,14 @@ class MyApp extends StatelessWidget {
                  radius: 50.0,
                  backgroundImage: AssetImage('images/rose-pink.jpeg'),
                ),
-               
+               Text('rose-pink',
+               style: TextStyle(
+                 fontFamily: 'Pacifico',
+                 fontSize: 40.0,
+                 color: Colors.white,
+                 fontWeight: FontWeight.bold,
+               ),
+               )
              ],
            ),
          ),
