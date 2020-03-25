753ad6336eaa10f5232f6f6d70dff4c27252b489
Naomi <naomi.ndibui@gmail.com>
2020-03-25 08:51:56 +0300

    padding of the container

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
              height: 100.0,
              width: 100.0,
              margin: EdgeInsets.only(left: 30.0),
+             padding: EdgeInsets.all(20.0),
              color: Colors.white,
              child: Text('Hello'),
            ),
